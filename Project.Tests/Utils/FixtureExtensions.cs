01399f5e9a641fcb430e6b36de261264de21af1e
Robert Dima <robert.dima@dnnsharp.com>
2018-04-05 23:28:37 +0300

    [Tests] Using AutoFixture to create SUT.

---
+++ Project.Tests/Utils/FixtureExtensions.cs
@@ -0,0 +1,49 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using AutoFixture.Dsl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Project.Tests.Utils {
+
+    internal class WebModelCustomization : CompositeCustomization {
+        internal WebModelCustomization()
+            : base(
+                new MvcCustomization(),
+                new AutoMoqCustomization() { ConfigureMembers = true }) {
+        }
+
+        private class MvcCustomization : ICustomization {
+            public void Customize(IFixture fixture) {
+                fixture.Customize<ControllerContext>(c => c
+                    .Without(x => x.DisplayMode));
+            }
+        }
+    }
+
+
+    internal static class FixtureExtensions {
+
+        internal static IPostprocessComposer<T> BuildController<T>(this IFixture fixture) where T : Controller {
+            return fixture.Build<T>()
+                .Without(c => c.ActionInvoker)
+                .Without(c => c.ControllerContext)
+                .Without(c => c.Resolver)
+                .Without(c => c.TempData)
+                .Without(c => c.TempDataProvider)
+                .Without(c => c.Url)
+                .Without(c => c.ValidateRequest)
+                .Without(c => c.ValueProvider)
+                .Without(c => c.ViewData)
+                .Without(c => c.ViewEngineCollection);
+        }
+
+        internal static T CreateController<T>(this IFixture fixture) where T : Controller {
+            return fixture.BuildController<T>().Create();
+        }
+    }
+}
