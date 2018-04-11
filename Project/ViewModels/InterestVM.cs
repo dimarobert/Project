81e38a411c7a75efc2eab132e5b682e03e91f85f
EmmaLeu <emma.leu28@gmail.com>
2018-04-11 22:34:46 +0300

    Add basic view models

---
+++ Project/ViewModels/InterestVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project.ViewModels
+{
+    public class InterestVM
+    {
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
